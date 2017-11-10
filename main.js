b06a00edb759643ff18e8d7008b78f727d260b12
Smikex <smikex@gmail.com>
2017-11-10 18:15:00 +0200

    first

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // modal window
 
-
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
@@ -16,3 +15,24 @@ window.onclick=function(event) {
 		modal.style.display = "none";
 	}
 }
+
+// Animation
+
+function myMove() {
+	var elem = document.getElementById("myAnimation");
+	var pos = 0;
+	var id = setInterval(frame, 10);
+	function frame() {
+		if (pos == 350){
+			clearInterval(id);
+		}else{
+			pos++;
+			elem.style.top = pos + 'px';
+			elem.style.left = pos + 'px';
+
+		}
+	}
+}
+
+// Slider
+
